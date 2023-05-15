81c41fd39821489e70410e5595f06715f69a9aa6
teewalk32 <teewalk4321@gmail.com>
2023-05-14 22:41:31 -0500

    added helpers folder. edit server.js

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
 const express = require('express');
-const path = require('path')
-// const fs = require("fs");
+const path = require('path');
+// const { clog } = require('./middleware/clog');
+const api = require('./routes/Index.js');
+const fs = require('fs');
 
-// calling express to work
-const app = express();
 
 // where local host port channel was picked
 const PORT = process.env.PORT || 3001;
 
+
+// calling express to work
+const app = express();
+
+// app.use(clog);
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
 app.use(express.static('./public'));
 
-// route for the server to the index.html
+//  get route for the server to the index.html homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/Index.html'))
 });
 
-// route for the notes.html
+// get route for the notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
 
-// express server on port 3001
+
 app.listen(PORT, () =>
     console.log(`Express Server on port ${PORT} !`)
 
