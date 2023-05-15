c499baafa33c50adad6d26805e101eae277083c2
teewalk32 <teewalk4321@gmail.com>
2023-05-14 21:50:12 -0500

    added routes for index and notes

---
+++ routes/Index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+
+const notesRouter = require('./notes');
+
+const app = express();
+
+app.use('/notes', notesRouter);
+
+
+module.exports = app;
---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db.json');
+    res.json(`Note added successfully!`);
+  } else {
+    res.error('Error in adding Note!');
+  }
+});
+
+module.exports = notes;
